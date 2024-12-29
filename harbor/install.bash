503fa5f17722d4e638e2d3d9edf885505c39ffc7
wjswhdgus123 <wjswhdgus123@naver.com>
2024-12-29 17:16:38 +0900

    new_repo

--- harbor/install.bash
+++
@@ -1,6 +0,0 @@
-helm repo add harbor https://helm.goharbor.io
-helm repo update
-helm pull harbor/harbor
-tar xvzf harbor-1.14.0.tgz
-kubectl patch storageclasses storege-harbornfs -p '{"metadata": {"annotations":{"storageclass.kuberetes.io/is-default-class":"true"}}}'
-helm install harbor -f values.yaml
