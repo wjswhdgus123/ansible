5f4a3927de820f15906ca20ffc8060f64e793bcd
wjswhdgus123 <wjswhdgus123@naver.com>
2025-01-12 17:25:40 +0900

    test

--- deploynode/k8sinstall.sh
+++ deploynode/k8sinstall.sh
@@ -11,4 +11,3 @@ echo "k8s node setting"
 sshpass -p '@whdgus123!' ansible-playbook -i /deploy/kubespray/inventory/onepredict/inventory.ini  -u ubuntu --ask-pass /deploy/ansible/setting/k8ssetting.yaml
 cd /deploy/kubespray
 sshpass -p '@whdgus123!' ansible-playbook  -i /deploy/kubespray/inventory/onepredict/inventory.ini cluster.yml  --forks 90 -b --become-user root -u ubuntu  -k
-sshpass -p '@whdgus123!' ansible-playbook  -i /deploy/kubespray/inventory/onepredict/inventory.ini /deploy/ansible/setting/devopssetup.yaml -u ubuntu -k  
