515d81cf1d5eded8d9f550271506806fa810f416
wjswhdgus123 <wjswhdgus123@naver.com>
2025-03-03 23:41:11 +0900

    test

--- deploynode/k8sinstall.sh
+++ deploynode/k8sinstall.sh
@@ -3,26 +3,23 @@
 echo "git Sync"
 cd /deploy/ansible
 git pull
+export ANSIBLE_BECOME_PASS="$PW"
 
 sed -i s/192.168.110.201/$IPA/g /deploy/ansible/deploynode/envconvert.yaml
 sed -i s/192.168.110.250/$VIP/g /deploy/ansible/deploynode/envconvert.yaml
-sed -i s/ubuntu/$TURBOUSER/g /deploy/ansible/deploynode/envconvert.yaml
-
-sed -i s/192.168.110.201/$IPA/g /deploy/ansible/setting/k8ssetting.yaml
-sed -i s/192.168.110.250/$VIP/g /deploy/ansible/setting/k8ssetting.yaml
-sed -i s/ubuntu/$TURBOUSER/g /deploy/ansible/setting/k8ssetting.yaml
+sed -i s/ubuntu/$USER/g /deploy/ansible/deploynode/envconvert.yaml
 
 
 sed -i s/192.168.110.201/$IPA/g /deploy/ansible/setting/k8ssetup.yaml
 sed -i s/192.168.110.250/$VIP/g /deploy/ansible/setting/k8ssetup.yaml
-sed -i s/ubuntu/$TURBOUSER/g /deploy/ansible/setting/k8ssetup.yaml
+sed -i s/ubuntu/$USER/g /deploy/ansible/setting/k8ssetup.yaml
 
 
 echo "envconvert"
 ansible-playbook /deploy/ansible/deploynode/envconvert.yaml
 
 echo "k8s node setting"
-sshpass -p $PW ansible-playbook -i /deploy/kubespray/inventory/onepredict/inventory.ini  -u $TURBOUSER --ask-pass /deploy/ansible/setting/k8ssetting.yaml
+sshpass -p $PW ansible-playbook -i /deploy/kubespray/inventory/onepredict/inventory.ini  /deploy/ansible/setting/k8ssetting.yaml  -u $USER -b --become-user root -k -K
 cd /deploy/kubespray
 sshpass -p $PW ansible-playbook  -i /deploy/kubespray/inventory/onepredict/inventory.ini cluster.yml  --forks 90 -b --become-user root -u $TURBOUSER  -k -K
 #sshpass -p $PW ansible-playbook -i /deploy/kubespray/inventory/onepredict/inventory.ini  -u $TURBOUSER --ask-pass /deploy/ansible/setting/k8ssetup.yaml
